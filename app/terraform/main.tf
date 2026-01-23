c22a204cfbbe8100fe5572665e5c8594efe5bc12
lzbluechip1 <lzbluechip1@gmail.com>
2026-01-23 14:08:18 +0530

    Update main.tf

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -1,16 +1,26 @@
 terraform {
-required_version = ">= 1.3.0"
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = ">= 4.0"
-}
-}
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.4.0"
+    }
+  }
 }
 
+provider "aws" {}
 provider "local" {}
 
 resource "local_file" "example" {
   filename = "hello.txt"
   content  = "Hello from Terraform MCP"
+
+  #  File permissions
+  #file_permission      = "0644"
+  # directory_permission = "0755"
 }
